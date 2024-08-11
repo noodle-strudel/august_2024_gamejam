3753d3caf298c576a9f8b56a36505b25e2b353ee
MrSqueeg <thagoglia@gmail.com>
2024-08-11 12:17:00 -0700

    Player now rotates to floor and removed unneeded code

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,7 @@ var gravDir : Vector2
 var playerInput : Vector2
 var perpendicular : Vector2
 var grounded = true
+var rotationAngle = 0.0
 
 # Movement Input
 func get_input():
@@ -24,13 +25,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Player Input -> movement mapping
+# Player Input -> movement mapping
 	playerInput = get_input()
+	
+	# Jump
 	if Input.get_action_strength("jump") > 0:
 		velocity = up_direction * speed
 		grounded = false
+	
+	# Reset
 	if playerInput == Vector2.ZERO:
 		perpendicular = Vector2.ZERO
+		
 	# Left
 	if playerInput.x < 0:
 		perpendicular.x = up_direction.y 
@@ -40,17 +46,7 @@ func _process(delta):
 	if playerInput.x > 0:
 		perpendicular.x = -up_direction.y 
 		perpendicular.y = -up_direction.x * -playerInput.x
-	
-	# Up
-	if playerInput.y < 0:
-		perpendicular = up_direction * grav
 		
-	# Down
-	if playerInput.y > 0:
-		perpendicular.y = -up_direction.y * playerInput.y
-		
-	print("Axis: ", perpendicular, "Up: ", up_direction)
-	print("Velocity: ", velocity, "\nperp: ", perpendicular)
 	# Apply Movement + Gravity when on ground
 	if grounded:
 		velocity = lerp(velocity, perpendicular * speed, delta * accel)
@@ -60,6 +56,8 @@ func _process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		grounded = true
+		rotation_degrees = rad_to_deg(atan2(up_direction.y, up_direction.x)) + 90
 		up_direction = collision.get_normal()
+		
 		velocity += (up_direction * (speed / 0.9) )
 
