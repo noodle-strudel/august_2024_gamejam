aff328e3a56b810ea9145c04a22c11ccfa6d27a1
MrSqueeg <thagoglia@gmail.com>
2024-08-11 11:54:07 -0700

    Player gravity movement and jumping

---
+++ Scripts/player.gd
@@ -0,0 +1,65 @@
+extends CharacterBody2D
+
+@export var grav = -500
+@export var speed = 750
+@export var accel = 10
+
+var input : Vector2
+var gravDir : Vector2
+var playerInput : Vector2
+var perpendicular : Vector2
+var grounded = true
+
+# Movement Input
+func get_input():
+	input.x = Input.get_action_strength("right") - Input.get_action_strength("left") 
+	input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	return input.normalized()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Player Input -> movement mapping
+	playerInput = get_input()
+	if Input.get_action_strength("jump") > 0:
+		velocity = up_direction * speed
+		grounded = false
+	if playerInput == Vector2.ZERO:
+		perpendicular = Vector2.ZERO
+	# Left
+	if playerInput.x < 0:
+		perpendicular.x = up_direction.y 
+		perpendicular.y = up_direction.x * playerInput.x
+	
+	# Right
+	if playerInput.x > 0:
+		perpendicular.x = -up_direction.y 
+		perpendicular.y = -up_direction.x * -playerInput.x
+	
+	# Up
+	if playerInput.y < 0:
+		perpendicular = up_direction * grav
+		
+	# Down
+	if playerInput.y > 0:
+		perpendicular.y = -up_direction.y * playerInput.y
+		
+	print("Axis: ", perpendicular, "Up: ", up_direction)
+	print("Velocity: ", velocity, "\nperp: ", perpendicular)
+	# Apply Movement + Gravity when on ground
+	if grounded:
+		velocity = lerp(velocity, perpendicular * speed, delta * accel)
+		velocity += up_direction * grav
+	
+	# Check for wall and change gravity direction
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		grounded = true
+		up_direction = collision.get_normal()
+		velocity += (up_direction * (speed / 0.9) )
+
