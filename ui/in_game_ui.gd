13dc328a9e37117c936de7a4c7b81d23c3d4f66e
palnex <thepalnex010@gmail.com>
2024-08-16 14:25:24 -0700

    UI POWER UP added - score label, escape button works for ui

--- ui/in_game_ui.gd
+++ ui/in_game_ui.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var GameManager = $"../GameManager"
+
+var on_pause_menu = true
+var on_setting_menu = false
 var is_loading_settings = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -68,22 +72,28 @@ func _on_back_pressed():
 	$ClickSFX.play()
 	$SettingsMenu.visible = !$SettingsMenu.visible
 	$inGameOptions.visible = !$inGameOptions.visible
+	on_pause_menu = true
+	on_setting_menu = false
 
 func _on_settings_pressed():
 	$ClickSFX.play()
 	$SettingsMenu.visible = !$SettingsMenu.visible
 	$inGameOptions.visible = !$inGameOptions.visible
+	on_pause_menu = false
+	on_setting_menu = true
 
 
 func _on_no_pressed():
 	$ClickSFX.play()
 	$inGameOptions.visible = !$inGameOptions.visible
 	$WarningBackMenu.visible = !$WarningBackMenu.visible
+	on_pause_menu = true
 
 
 func _on_yes_pressed():
 	$ClickSFX.play()
-	#await get_tree().create_timer(0.3).timeout
+	Engine.time_scale = 1
+	await get_tree().create_timer(0.3).timeout
 	get_tree().change_scene_to_file("res://ui/start_ui.tscn")
 
 
@@ -91,6 +101,7 @@ func _on_back_menu_pressed():
 	$ClickSFX.play()
 	$inGameOptions.visible = !$inGameOptions.visible
 	$WarningBackMenu.visible = !$WarningBackMenu.visible
+	on_pause_menu = false
 
 
 func _on_music_slider_value_changed(value):
@@ -109,3 +120,8 @@ func _on_sfx_slider_value_changed(value):
 
 func _on_about_to_quit():
 	GlobalSettings.save()
+
+
+func _on_resume_pressed():
+	$ClickSFX.play()
+	GameManager._pause_menu()
