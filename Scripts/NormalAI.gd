cade7432404e92de87c4b42ff034d2ec258a3b1b
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2024-08-18 12:23:03 -0700

    normal ai movement

--- Scripts/NormalAI.gd
+++ Scripts/NormalAI.gd
@@ -7,17 +7,33 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var ball = $"/root/MainGame/Ball"
+
+# Raycast for the ball detection
+@onready var raycast = $RayCast2D
+
+@onready var ball = $"/root/MainGame/Ball" 
 
 # Overriding the input for ai
 func get_input():
 	if(grounded):
-		print(ball)
-		#input.x = x
-		#input.y = 0
+		ball = ball as RigidBody2D
+		var angle = rad_to_deg(self.position.angle_to(ball.position))
 		
-	else:
-		pass
+		print(angle)
+		if(angle > 0):
+			input.x = -1
+		else:
+			input.x = 1
+	
+	# Get the first collision object
+	var raycast_collision = raycast.get_collider()
+	
+	## Detecting the ball in front to jump towards
+	if raycast_collision != null and raycast_collision.get_name() == "Ball":
+		pressing_jump = 1
+	else: 
+		pressing_jump = 0
+		
 	
 	
 	# Get the first collision object
