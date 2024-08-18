8cdcae3a841d80f092805f7f72a5b74b0b5ae0aa
noodle-strudel <yollaine369@gmail.com>
2024-08-18 12:34:27 -0700

    ghost now resets itself each round reset

--- Scripts/ghostInputReplay.gd
+++ Scripts/ghostInputReplay.gd
@@ -42,6 +42,7 @@ func _ready():
 	jumpTimer.wait_time = 0
 	jumpTimer.timeout.connect(_jumptimer_Timeout)
 	add_child(jumpTimer)
+	get_tree().current_scene.get_node("Ball").resetRound.connect(_on_reset_round)
 
 	
 # Player Movement proccessing and collisions
