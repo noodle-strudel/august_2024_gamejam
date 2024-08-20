18c6ce0acd6803c9d447104ea52144d93402166e
noodle-strudel <yollaine369@gmail.com>
2024-08-20 12:51:14 -0700

    adding the function to delete when the game is run once

--- ui/globalSettings.gd
+++ ui/globalSettings.gd
@@ -8,6 +8,9 @@ var resolution: int
 var window: int
 var difficulty: int #0 easy, 1 normal, 2 difficult
 
+func _ready():
+	delete_save_file()
+	
 func save():
 	var file = FileAccess.open(save_path, FileAccess.WRITE)
 	file.store_var(volume)
